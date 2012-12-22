cf17b2dd0b61a01435690fa5be31a36ea84d8eed
Ismael Gorissen <ismael.gorissen@pinchproject.com>
2012-12-22 11:31:29 +0100

    added private function to make all requests to xboxapi.com

--- libs/xbox.api.js
+++ libs/xbox.api.js
@@ -1,8 +1,20 @@
+/**
+ * Required modules.
+ */
 var https = require('https');
 
-var XboxApi = function XboxApi() {
-    var _api_limit = 150;
 
+/**
+ * @constructor
+ */
+var XboxApi = function XboxApi() {
+
+    /**
+     * Retrieve the profile for a gamertag.
+     *
+     * @param gamertag
+     * @param callback
+     */
     this.profile = function (gamertag, callback) {
         if (gamertag) {
             var opts = {
@@ -10,44 +22,26 @@ var XboxApi = function XboxApi() {
                 path:'/profile/' + gamertag
             };
 
-            var req = https.request(opts, function (res) {
-                var data = '';
-
-                res.on('data', function (chunk) {
-                    data += chunk;
-                });
-
-                res.on('end', function () {
-                    try {
-                        var json = JSON.parse(data);
-
-                        if (limitCheck(json.API_Limit)) {
-                            if (json.Success == true) {
-                                callback(json);
-                            } else {
-                                callback(null, json.Error);
-                            }
-                        } else {
-                            callback(null, new Error('API Limit has been reached'));
-                        }
-                    }
-                    catch (e) {
-                        callback(e);
-                    }
-                });
+            fetchData(opts, function (data, err) {
+                if (err) {
+                    callback(null, err);
+                } else {
+                    callback(data, null);
+                }
             });
-
-            req.on('error', function (err) {
-                callback(null, err);
-            });
-
-            req.end();
         }
         else {
             callback(null, new Error('Undefined gamertag'));
         }
     }
 
+
+    /**
+     * retrieve the friend list for a gamertag.
+     *
+     * @param gamertag
+     * @param callback
+     */
     this.friends = function (gamertag, callback) {
         if (gamertag) {
             var opts = {
@@ -55,44 +49,26 @@ var XboxApi = function XboxApi() {
                 path:'/friends/' + gamertag
             };
 
-            var req = https.request(opts, function (res) {
-                var data = '';
-
-                res.on('data', function (chunk) {
-                    data += chunk;
-                });
-
-                res.on('end', function () {
-                    try {
-                        var json = JSON.parse(data);
-
-                        if (limitCheck(json.API_Limit)) {
-                            if (json.Success == true) {
-                                callback(json);
-                            } else {
-                                callback(null, json.Error);
-                            }
-                        } else {
-                            callback(null, new Error('API Limit has been reached'));
-                        }
-                    }
-                    catch (e) {
-                        callback(e);
-                    }
-                });
+            fetchData(opts, function (data, err) {
+                if (err) {
+                    callback(null, err);
+                } else {
+                    callback(data, null);
+                }
             });
-
-            req.on('error', function (err) {
-                callback(null, err);
-            });
-
-            req.end();
         }
         else {
             callback(null, new Error('Undefined gamertag'));
         }
     }
 
+
+    /**
+     * Retrieve the game list for a gamertag.
+     *
+     * @param gamertag
+     * @param callback
+     */
     this.games = function (gamertag, callback) {
         if (gamertag) {
             var opts = {
@@ -100,108 +76,128 @@ var XboxApi = function XboxApi() {
                 path:'/games/' + gamertag
             };
 
-            var req = https.request(opts, function (res) {
-                var data = '';
-
-                res.on('data', function (chunk) {
-                    data += chunk;
-                });
-
-                res.on('end', function () {
-                    try {
-                        var json = JSON.parse(data);
-
-                        if (limitCheck(json.API_Limit)) {
-                            if (json.Success == true) {
-                                callback(json);
-                            } else {
-                                callback(null, json.Error);
-                            }
-                        } else {
-                            callback(null, new Error('API Limit has been reached'));
-                        }
-                    }
-                    catch (e) {
-                        callback(e);
-                    }
-                });
+            fetchData(opts, function (data, err) {
+                if (err) {
+                    callback(null, err);
+                } else {
+                    callback(data, null);
+                }
             });
-
-            req.on('error', function (err) {
-                callback(null, err);
-            });
-
-            req.end();
         }
         else {
             callback(null, new Error('Undefined gamertag'));
         }
     }
 
-    this.achievements = function (gamertag, game, callback) {
+
+    /**
+     * Retrieve the achievement list of a game for a gamertag.
+     *
+     * @param gamertag
+     * @param gameId
+     * @param callback
+     */
+    this.achievements = function (gamertag, gameId, callback) {
         if (gamertag) {
-            if (game) {
+            if (gameId) {
                 var opts = {
                     host:'xboxapi.com',
-                    path:'/achievements/' + game + '/' + gamertag
+                    path:'/achievements/' + gameId + '/' + gamertag
                 };
 
-                var req = https.request(opts, function (res) {
-                    var data = '';
-
-                    res.on('data', function (chunk) {
-                        data += chunk;
-                    });
-
-                    res.on('end', function () {
-                        try {
-                            var json = JSON.parse(data);
-
-                            if (limitCheck(json.API_Limit)) {
-                                if (json.Success == true) {
-                                    callback(json);
-                                } else {
-                                    callback(null, json.Error);
-                                }
-                            } else {
-                                callback(null, new Error('API Limit has been reached'));
-                            }
-                        }
-                        catch (e) {
-                            callback(e);
-                        }
-                    });
+                fetchData(opts, function (data, err) {
+                    if (err) {
+                        callback(null, err);
+                    } else {
+                        callback(data, null);
+                    }
                 });
-
-                req.on('error', function (err) {
-                    callback(null, err);
-                });
-
-                req.end();
             } else {
                 callback(null, new Error('Undefined game ID'));
             }
-        }
-        else {
+        } else {
             callback(null, new Error('Undefined gamertag'));
         }
     }
 
-    function limitCheck(limit) {
-        var result = limit.split('/');
 
-        var value = parseInt(result[0]);
+    /**
+     * Fetch the JSON data from xboxapi.com.
+     *
+     * @param opts
+     * @param callback
+     */
+    function fetchData(opts, callback) {
+        var req = https.request(opts, function (res) {
+            var data = '';
 
-        return value < _api_limit;
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function () {
+                try {
+                    var json = JSON.parse(data);
+
+                    if (limitCheck(json.API_Limit)) {
+                        if (json.Success == true) {
+                            callback(json, null);
+                        } else {
+                            callback(null, json.Error);
+                        }
+                    } else {
+                        callback(null, new Error('API Limit has been reached'));
+                    }
+                }
+                catch (e) {
+                    callback(null, e);
+                }
+            });
+
+            res.on('error', function (err) {
+                callback(null, err);
+            });
+        });
+
+        req.on('error', function (err) {
+            callback(null, err);
+        });
+
+        req.end();
     }
 
+
+    /**
+     * Check if the limit of requests per hour is reached.
+     *
+     * @param apiLimit
+     * @return {Boolean}
+     */
+    function limitCheck(apiLimit) {
+        var result = apiLimit.split('/');
+
+        var current = parseInt(result[0]),
+            limit = parseInt(result[1]);
+
+        return current < limit;
+    }
+
+
+    // Check if we try to create a new instance
     if (XboxApi.caller != XboxApi.getInstance) {
         throw new Error('A new instance can not be set for this object');
     }
 }
 
+
 XboxApi.instance = null;
 
+
+/**
+ * Create or retrieve the scraper instance.
+ *
+ * @return {*}
+ */
 XboxApi.getInstance = function () {
     if (this.instance === null) {
         this.instance = new XboxApi();
@@ -209,5 +205,6 @@ XboxApi.getInstance = function () {
 
     return this.instance;
 }
+
 
 module.exports = XboxApi.getInstance();
